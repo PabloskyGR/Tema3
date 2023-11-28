f8b44d7b8157b0c801cc71be2658837280dc4000
jpgonzalez <jpgonzalez@106-03.info2>
2023-11-28 13:50:53 +0100

    3 commit

--- Tema3/src/ejercicio5/TablaMultiplicar.java
+++ Tema3/src/ejercicio5/TablaMultiplicar.java
@@ -5,7 +5,7 @@ public class TablaMultiplicar {
 	public static void tablaMultiplicar(int num) {
 		
 		for(int i = 1; i <= 10; i++) {
-			System.out.println(i + " * " + num + " = " + (i*num));
+			System.out.println(i + " * " + num + " = " + (i * num));
 		}
 	}
 
